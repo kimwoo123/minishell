02f3c6a5fca1cc1bc09ce3c2c095d19a0be6957b
CM <jcm0623@naver.com>
2023-03-23 10:02:46 +0900

    FIX: home dir problem

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -12,6 +12,22 @@
 
 #include "minishell.h"
 
+static int	change_dir_nothing(t_data *data)
+{
+	char	*path;
+	
+	path = find_home_path(data->copied_envp);
+	if (path == NULL)
+	{
+		ft_putendl_fd("cd: HOME not set", STDERR_FILENO);
+		set_status(EXIT_FAILURE);
+		return (SUCCESS);
+	}
+	ft_chdir(path, path);
+	// free(path);
+	return (SUCCESS);
+}
+
 static int	get_home_path(t_data *data, char **home_path)
 {
 	*home_path = find_home_path(data->envp);
@@ -30,19 +46,19 @@ static int	get_home_path(t_data *data, char **home_path)
 	return (TRUE);
 }
 
-static int	change_directory_to_home(t_data *data)
+static int	change_dir_to_home(t_data *data)
 {
 	char	*home_path;
 	char	*path;
-	int		result;
 
-	result = get_home_path(data, &home_path);
-	if (result == FALSE)
-		return (FAILURE);
-	else if (result == FAILURE)
-		return (SUCCESS);
-	if (data->commands[1] == NULL \
-	|| !ft_strncmp(data->commands[1], "~", ft_strlen(data->commands[1])) \
+	// result = get_home_path(data, &home_path);
+	// if (result == FALSE)
+	// 	return (FAILURE);
+	// else if (result == FAILURE)
+	// 	return (SUCCESS);
+
+	home_path = find_home_path(data->envp);
+	if (!ft_strncmp(data->commands[1], "~", ft_strlen(data->commands[1])) \
 	|| !ft_strncmp(data->commands[1], "~/", ft_strlen(data->commands[1])))
 		ft_chdir(home_path, data->commands[1]);
 	else
@@ -56,7 +72,7 @@ static int	change_directory_to_home(t_data *data)
 	return (SUCCESS);
 }
 
-static int	change_directory_to_root(char **commands)
+static int	change_dir_to_root(char **commands)
 {
 	char	*path;
 
@@ -73,7 +89,7 @@ static int	change_directory_to_root(char **commands)
 	return (SUCCESS);
 }
 
-static int	change_directory(char **commands)
+static int	change_dir(char **commands)
 {
 	char	*current_path;
 	char	*path;
@@ -90,30 +106,31 @@ static int	change_directory(char **commands)
 	return (SUCCESS);
 }
 
+static int	change_directory(t_data *data)
+{
+	int	result;
+
+	set_status(EXIT_SUCCESS);
+	result = SUCCESS;
+	if (data->commands[1] == NULL)
+		result = change_dir_nothing(data);
+	else if (!ft_strncmp(data->commands[1], "~", ft_strlen("~")))
+		result = change_dir_to_home(data);
+	else if (!ft_strncmp(data->commands[1], "/", ft_strlen("/")))
+		result = change_dir_to_root(data->commands);
+	else
+		result = change_dir(data->commands);
+	return (result);
+}
+
 void	cd_command(t_data *data)
 {
 	char	*path;
-	char	*temp;
-	char	*pwd_path;
 
 	if (backup_working_directory(data) == FAILURE)
-		exit_with_str("error in cd command", EXIT_FAILURE);
-	if (data->commands[1] == NULL \
-	|| !ft_strncmp(data->commands[1], "~", ft_strlen("~")))
-	{
-		if (change_directory_to_home(data) == FAILURE)
-			exit_with_str("error in cd command", EXIT_FAILURE);
-	}
-	else if (!ft_strncmp(data->commands[1], "/", ft_strlen("/")))
-	{
-		if (change_directory_to_root(data->commands) == FAILURE)
-			exit_with_str("error in cd command", EXIT_FAILURE);
-	}
-	else
-	{
-		if (change_directory(data->commands) == FAILURE)
-			exit_with_str("error in cd command", EXIT_FAILURE);
-	}
+		exit_with_str("backup error in cd command", EXIT_FAILURE);
+	if (change_directory(data) == FAILURE)
+		exit_with_str("cd error in cd command", EXIT_FAILURE);
 	if (change_working_directory(data) == FAILURE)
-		exit_with_str("error in cd command", EXIT_FAILURE);
+		exit_with_str("backup error in cd command", EXIT_FAILURE);
 }
