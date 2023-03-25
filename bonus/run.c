0a820ab3d8573523cf9d3defb1a57ee264a4b019
CM <jcm0623@naver.com>
2023-03-25 11:01:49 +0900

    FIX: now we can work with logical operators

--- bonus/run.c
+++ bonus/run.c
@@ -61,16 +61,7 @@ void	waiting(t_data *data)
 
 void	recursive_make_tree_tester(t_tree **head, t_list *node, t_list **temp)
 {
-	// dprintf(2, "---------------------\n");
-	// dprintf(2, "type: %d\n", node->type);
-	// dprintf(2, "cont: %s\n", node->content);
-	// dprintf(2, "---------------------\n");
-	if (node == NULL)
-	{
-		*temp = node;
-		return ;
-	}
-	if (node->type == OPERATOR)
+	if (node == NULL || node->type == OPERATOR)
 	{
 		*temp = node;
 		return ;
@@ -102,45 +93,28 @@ t_tree	*make_tree_before_operator(t_list **addr)
 		exit_with_str("malloc error in make tree", EXIT_FAILURE);
 	recursive_make_tree_tester(&head, *addr, &temp);
 	*addr = temp;
-	// temp = node;
-	// dprintf(2, "---------------------\n");
-	// dprintf(2, "type: %d\n", temp->type);
-	// dprintf(2, "cont: %s\n", temp->content);
-	// dprintf(2, "next: %p\n", temp->next);
-	// dprintf(2, "---------------------\n");
-	// dprintf(2, "---------------------\n");
-	// dprintf(2, "type: %d\n", temp->next->type);
-	// dprintf(2, "cont: %s\n", temp->next->content);
-	// dprintf(2, "next: %p\n", temp->next->next);
-	// dprintf(2, "---------------------\n");
 	return (head);
 }
 
 void	make_tree_tester(t_data *data, t_list **addr)
 {
-	t_tree *tree;
-	int		flag;
+	extern int	g_status;
+	t_tree		*tree;
+	int			flag;
 
+	flag = TRUE;
 	if (init_backup(data) == FAILURE)
 		exit_with_str("backup error in run minishell", EXIT_FAILURE);
 	if ((*addr)->type == OPERATOR)
 	{
-		flag = TRUE;
-
-		// if (is_equal_to((*addr)->content, AND)
-
-
+		if ((is_equal_to((*addr)->content, AND) && g_status == EXIT_FAILURE) \
+		|| (is_equal_to((*addr)->content, OR) && g_status == EXIT_SUCCESS))
+			flag = FALSE;
 		(*addr) = (*addr)->next;
-		tree = make_tree_before_operator(addr);
-		// if (flag == TRUE)
-		search_tree_for_hd(data, tree);
-		search_tree(data, tree);
-		waiting(data);
 	}
-	else
+	tree = make_tree_before_operator(addr);
+	if (flag == TRUE)
 	{
-
-		tree = make_tree_before_operator(addr);
 		search_tree_for_hd(data, tree);
 		search_tree(data, tree);
 		waiting(data);
@@ -166,30 +140,3 @@ void	run_minishell(t_data *data, char *command_line)
 		free_list(&list);
 	}
 }
-
-// void	run_minishell(t_data *data, char *command_line)
-// {
-// 	t_list	*list;
-// 	t_tree	*tree;
-
-// 	data->count_cmd = 0;
-// 	data->pid = 0;
-// 	data->last_cmd = FALSE;
-// 	data->has_forked = FALSE;
-// 	if (backup_stdio(data) == FAILURE)
-// 		exit_with_str("backup error in run minishell", EXIT_FAILURE);
-// 	list = scan_command(command_line, data);
-// 	if (list == NULL)
-// 		rl_on_new_line();
-// 	else
-// 	{	
-// 		tree = make_tree(&list);
-// 		search_tree_for_hd(data, tree);
-// 		search_tree(data, tree);
-// 		waiting(data);
-// 		free_list(&list);
-// 		free_tree(tree);
-// 	}
-// 	if (restore_stdio(data) == FAILURE)
-// 		exit_with_str("restore error in run minishell", EXIT_FAILURE);
-// }
